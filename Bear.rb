371023b17aba95e444ffbeeb1f4509279e61ee10
Chris Marshall <cmarshall10450@gmail.com>
2017-10-03 16:30:44 +0100

    Bear - test_take_fish_from_river passing

--- Bear.rb
+++ Bear.rb
@@ -1,10 +1,15 @@
 class Bear
 
-	attr_reader :name
+	attr_reader :name, :stomach
 
 	def initialize(name)
 		@name = name
 		@stomach = []
+	end
+
+	def take_fish_from_river(river)
+		fish = river.fish.pop()
+		@stomach.push(fish)
 	end
 
 end
--- specs/Bear.spec.rb
+++ specs/Bear.spec.rb
@@ -1,13 +1,23 @@
 require('minitest/autorun')
 require_relative('../Bear')
+require_relative('../River')
+require_relative('../Fish')
 
 class TestBear < MiniTest::Test
 
 	def setup
+		@fish = Fish.new('Blinky')
+		@river = River.new('Forth', [@fish])
 		@bear = Bear.new('Yogi')
 	end
 
 	def test_get_bear_name
 		assert_equal('Yogi', @bear.name)
+	end
+
+	def test_take_fish_from_river
+		@bear.take_fish_from_river(@river)
+		assert_equal([@fish], @bear.stomach)
+		assert_equal([], @river.fish)
 	end
 end
